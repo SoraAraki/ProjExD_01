5af81ebbcc07ebc930ede6a93c66432dd8ab34ef
Araki <c0b220062b@edu.teu.ac.jp>
2023-06-13 15:42:59 +0900

    練習3

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,8 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(pg.image.load("ex01/fig/3.png"),True,False)
-   
+    kk_img = pg.transform.rotozoom(kk_img,10,1.0)
+     
     tmr = 0
     while True:
         for event in pg.event.get():
